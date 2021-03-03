cb63cc9469fbc083a498a16a8b464b926d07931f
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-03-03 09:20:00 -0300

    test [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -8,30 +8,7 @@ if
 fi
 
 
-echo "Build pulp/pulpcore images"
-cd $GITHUB_WORKSPACE/containers/
-cp $GITHUB_WORKSPACE/.ci/ansible/vars.yaml vars/vars.yaml
-sed -i "s/podman/docker/g" common_tasks.yaml
-pip install ansible
-ansible-playbook -v build.yaml
-cd $GITHUB_WORKSPACE
-
-echo "Test pulp/pulpcore images"
-sudo -E ./up.sh
-.ci/scripts/pulp-operator-check-and-wait.sh $KUBE_FLAG
-.ci/scripts/pulp_file-tests.sh -m
-
-docker images
-
-echo "Deploy pulp latest"
-sudo -E QUAY_REPO_NAME=pulp $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
-echo "Deploy pulpcore latest"
-sudo -E QUAY_REPO_NAME=pulpcore $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
-echo "Deploy pulp-web latest"
-sudo -E QUAY_REPO_NAME=pulp-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
 echo "Deploy pulp-operator"
-sudo -E operator-sdk build quay.io/pulp/pulp-operator:latest
+# sudo -E operator-sdk build quay.io/pulp/pulp-operator:latest
+eval $(minikube -p minikube docker-env)
 sudo -E $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh

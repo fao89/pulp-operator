38bb77a59e09f63f0dbf4f992acd3ae7f15fae40
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-05-24 14:15:44 -0300

    Bump pulpcore on galaxy
    [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -23,12 +23,6 @@ else
 fi
 cd $GITHUB_WORKSPACE
 
-echo "Deploy pulp latest"
-sudo -E QUAY_REPO_NAME=pulp $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
-echo "Deploy pulpcore latest"
-sudo -E QUAY_REPO_NAME=pulpcore $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
-
 echo "Deploy galaxy latest"
 sudo -E QUAY_REPO_NAME=galaxy $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
 
@@ -46,19 +40,6 @@ else
   ansible-playbook -v build.yaml --extra-vars "quay_expire=${QUAY_EXPIRE}"
 fi
 cd $GITHUB_WORKSPACE
-
-echo "Test pulp/pulpcore images"
-if [[ -n "${QUAY_EXPIRE}" ]]; then
-  echo "LABEL quay.expires-after=${QUAY_EXPIRE}d" >> ./build/Dockerfile
-fi
-sudo -E ./up.sh
-.ci/scripts/pulp-operator-check-and-wait.sh $KUBE_FLAG
-.ci/scripts/retry.sh 3 ".ci/scripts/pulp_file-tests.sh -m"
-
-docker images
-
-echo "Deploy pulp-web latest"
-sudo -E QUAY_REPO_NAME=pulp-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
 
 echo "Deploy galaxy-web latest"
 sudo -E QUAY_REPO_NAME=galaxy-web $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh

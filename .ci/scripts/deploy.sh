5c25413430d9d19935bd8d9560bc0bc548593634
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-06 16:09:03 -0300

    Deploy [noissue]

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -45,18 +45,6 @@ else
 fi
 cd $GITHUB_WORKSPACE
 
-echo "Test pulp/pulpcore images"
-if [[ -n "${QUAY_EXPIRE}" ]]; then
-  echo "LABEL quay.expires-after=${QUAY_EXPIRE}d" >> ./build/Dockerfile
-fi
-sudo -E ./up.sh
-.ci/scripts/pulp-operator-check-and-wait.sh $KUBE_FLAG
-if [[ "$CI_TEST" == "galaxy" ]]; then
-  CI_TEST=true .ci/scripts/galaxy_ng-tests.sh -m
-else
-  .ci/scripts/retry.sh 3 ".ci/scripts/pulp_file-tests.sh -m"
-fi
-
 docker images
 
 if [[ "$CI_TEST" == "galaxy" ]]; then

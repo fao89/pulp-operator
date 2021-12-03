1910a86695ad2b04d687257d49e935ff3645a145
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-12-03 17:45:44 +0000

    test [noissue]

--- .ci/scripts/quay-push.sh
+++ .ci/scripts/quay-push.sh
@@ -6,7 +6,7 @@
 
 # TODO: These are already hardcoded in .travis.yml for the build task
 #
-# Pulp is an organization (not an individual user account) on Quay:
+# Pulp is an organization (not an individual user account) on Quay
 # https://quay.io/organization/pulp
 # For test publishes, one can override this to any org or user.
 QUAY_PROJECT_NAME=${QUAY_PROJECT_NAME:-pulp}
@@ -15,7 +15,7 @@ QUAY_REPO_NAME=${QUAY_REPO_NAME:-pulp-operator}
 # The image tag
 QUAY_IMAGE_TAG=${QUAY_IMAGE_TAG:-latest}
 
-QUAY_BOT_USERNAME=${QUAY_BOT_USERNAME:-pulp+github}
+QUAY_BOT_USERNAME=${QUAY_BOT_USERNAME:-fabricio_aguiar}
 
 # Reference: https://adriankoshka.github.io/blog/posts/travis-and-quay/
 echo "$QUAY_BOT_PASSWORD" | docker login -u "$QUAY_BOT_USERNAME" --password-stdin quay.io
